5250fc50fe6c443b44937b3762f9510eb8b8762e
Omar <tayeho@oulook.com>
2023-06-14 11:46:58 +0100

    Refactoring App to use Redux using Redux Toolkit

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,28 +1,18 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
-import { useStoreContext } from "../../app/context/StoreContext";
 import { currencyFormat } from "../../app/util/util";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props {
     product: Product;
 }
 
 export default function ProductCard({product}: Props) {
-  const [loading, setLoading] = useState(false);
-  const {setBasket} = useStoreContext();
-
-  function handleAddItem(productId: number) {
-    setLoading(true);
-    agent.Basket.addItem(productId)
-      .then(response => setBasket(response.value))
-      .catch(error => console.log(error))
-      .finally(() => setLoading(false))
-  }
-
+  const {status} = useAppSelector(state => state.basket);
+  const dispatch = useAppDispatch();
     return(
       <Card>
         <CardHeader 
@@ -50,7 +40,7 @@ export default function ProductCard({product}: Props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
+        <LoadingButton loading={status.includes('pendingAddItem' + product.id)} onClick={() => dispatch(addBasketItemAsync({productId: product.id}))} size="small">Add to cart</LoadingButton>
         <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
